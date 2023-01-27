c0044dd116e56f3d92f204d2fcaba44aebe81eb8
jang010505 <jang010505@inu.ac.kr>
2023-01-27 22:41:56 +0900

    feat: Init create function, Add SLf4j lombok

--- src/main/java/com/example/noticeBoard/Controller/noticeBoardController.java
+++ src/main/java/com/example/noticeBoard/Controller/noticeBoardController.java
@@ -1,12 +1,23 @@
 package com.example.noticeBoard.Controller;
 
 import com.example.noticeBoard.dto.BoardForm;
+import com.example.noticeBoard.entity.Board;
+import com.example.noticeBoard.repository.BoardRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+
 @Controller
+@Slf4j
 public class noticeBoardController {
+
+    @Autowired
+    private BoardRepository boardRepository;
 
     @GetMapping("main")
     public String main(){
@@ -14,14 +25,27 @@ public class noticeBoardController {
     }
 
     @GetMapping("show")
-    public String show() { return "board/show"; }
+    public String show(Model model) {
+        ArrayList<Board> boardEntityList = boardRepository.findAll();
+
+        model.addAttribute("boardList", boardEntityList);
+
+        return "board/show";
+    }
 
     @GetMapping("new")
     public String newForm() {return "board/new"; }
 
     @PostMapping("create")
     public String create(BoardForm form) {
-        System.out.println(form.toString());
-        return null;
+        log.info(form.toString());
+
+        Board board = form.toEntity();
+        log.info(board.toString());
+
+        Board saved = boardRepository.save(board);
+        log.info(saved.toString());
+
+        return "redirect:main";
     }
 }
